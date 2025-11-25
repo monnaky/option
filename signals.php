5e892ac6250c9c91dc82ea7088b8ce043ee4e97c
monnaky <102661728+monnaky@users.noreply.github.com>
2025-11-25 02:29:08 +0100

    Create signals.php

---
+++ signals.php
@@ -0,0 +1,14 @@
+<?php
+// signals.php - Temporary connection test
+if (empty($_POST["text"])) die("failed... text not specified...");
+
+$text = $_POST["text"];
+
+$file = fopen("getSignal.txt", "w") or die("failed... unable to open file");
+
+fwrite($file, $text);
+
+fclose($file);
+
+echo "success: " . $text;
+?>
