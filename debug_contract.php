52df98e75b2bab272fe8624fd778a96b874fed58
Rootcreator <pluggedspace@gmail.com>
2025-12-12 19:06:43 +0100

    Update debug_contract.php

--- debug_contract.php
+++ debug_contract.php
@@ -9,7 +9,6 @@
 
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/app/bootstrap.php';
-require_once __DIR__ . '/app/services/DerivAPI.php';
 
 use App\Config\Database;
 use App\Services\TradingBotService;
